c4e57d76b57e617455ed30c71b4cb9404e44bfc6
azskrzysiek <azskrzysiek@gmail.com>
2019-05-14 16:13:17 +0200

    Add policies, show post

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,13 +4,18 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://scontent-waw1-1.cdninstagram.com/vp/d1db36f0d9ab1b61ed4aaafec84a12b1/5D655738/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com" class="rounded-circle" alt="">
+            <img src="/storage/{{ $user->profile->image}}" class="rounded-circle w-100" alt="">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="/p/create">Add new Post</a>
+                @can('update', $user->profile)
+                    <a href="/p/create">Add new Post</a>
+                @endcan
             </div>
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit profile</a>
+            @endcan
             <div class="d-flex">
             <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> fallowers</div>
@@ -26,7 +31,9 @@
     <div class="row">
         @foreach ($user->posts as $post)
         <div class="col-4 pt-5">
-            <img src="/storage/{{ $post->image }}" class="w-100">
+            <a href="/p/{{ $post->id }}">
+                <img src="/storage/{{ $post->image }}" class="w-100">
+            </a>
         </div>
         @endforeach
         
